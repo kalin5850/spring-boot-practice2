77071d4ae0f5255d68fc01615247a2c8c87eaa51
KaiLin Chang <kalin5850@gmail.com>
2024-01-17 18:07:11 -0500

    add postgresql connector, username and password on application.properties

--- src/main/java/com/pykaicode/cruddemo/CruddemoApplication.java
+++ src/main/java/com/pykaicode/cruddemo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.pykaicode.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +12,8 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> System.err.println("Hello World");
+	}
 }
