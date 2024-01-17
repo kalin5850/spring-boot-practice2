666f8714899c38c58b20a97563818b857e02ebdd
KaiLin Chang <kalin5850@gmail.com>
2024-01-17 17:17:58 -0500

    init cruddemo project

---
+++ src/test/java/com/pykaicode/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pykaicode.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
