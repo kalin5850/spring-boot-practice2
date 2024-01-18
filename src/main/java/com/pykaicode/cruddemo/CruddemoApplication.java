86c0f4d170da3a0021e296c21fcf2af9b9c3bac5
KaiLin Chang <kalin5850@gmail.com>
2024-01-18 00:58:32 -0500

    use faker and insert multiple student objects

--- src/main/java/com/pykaicode/cruddemo/CruddemoApplication.java
+++ src/main/java/com/pykaicode/cruddemo/CruddemoApplication.java
@@ -1,10 +1,14 @@
 package com.pykaicode.cruddemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.github.javafaker.Faker;
 import com.pykaicode.cruddemo.dao.StudentDAO;
 import com.pykaicode.cruddemo.entity.Student;
 
@@ -17,19 +21,38 @@ public class CruddemoApplication {
 
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
-		return runner -> createStudent(studentDAO);
+		;
+		return runner -> createMultipleStudents(studentDAO);
 	}
 
-	private void createStudent(StudentDAO studentDAO) {
-
-		// create the student object
-		System.out.println("Creating a new student object....");
-		Student tempStudent = new Student("KaiLin",
-				"Chang", "daokai0509@gmail.com");
-		// save the student object
-		System.out.println("Saving the student...");
-		studentDAO.save(tempStudent);
-		// display id of the saved student
-		System.err.println("Saved student. Generated id: " + tempStudent.getId());
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		Faker faker = new Faker();
+		List<Student> students = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			Student tmpStudent = new Student(
+					faker.name().firstName(),
+					faker.name().lastName(),
+					faker.internet().emailAddress());
+			students.add(tmpStudent);
+			studentDAO.save(tmpStudent);
+		}
 	}
+
+	// @Bean
+	// public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+	// return runner -> createStudent(studentDAO);
+	// }
+
+	// private void createStudent(StudentDAO studentDAO) {
+
+	// // create the student object
+	// System.out.println("Creating a new student object....");
+	// Student tempStudent = new Student("KaiLin",
+	// "Chang", "daokai0509@gmail.com");
+	// // save the student object
+	// System.out.println("Saving the student...");
+	// studentDAO.save(tempStudent);
+	// // display id of the saved student
+	// System.err.println("Saved student. Generated id: " + tempStudent.getId());
+	// }
 }
